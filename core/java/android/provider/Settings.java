ee4f9ba4431c76a7225882904322fc279a4e1418
Daxxmax <>
2015-01-06 08:47:44 +0100

    FW_Base - Statusbar clock font style (1/2)
    Change-Id: Iafb811b9f819a100940fdeeaa2c657483c039950
    Signed-off-by: STELIX <ssspinni@gmail.com>
    Signed-off-by: W4TCH0UT <ateekujjawal@gmail.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4095,6 +4095,12 @@ public final class Settings {
         public static final String FORMAL_TEXT_INPUT = "formal_text_input";
 
         /**
+         * Settings for clock font style
+         * @hide
+         */
+        public static final String STATUSBAR_CLOCK_FONT_STYLE = "statusbar_clock_font_style";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
