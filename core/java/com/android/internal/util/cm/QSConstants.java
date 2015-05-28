044c01f8c2c6dfccd53c9b86f2c623d3ade80871
OwnDroid <askowndroid@gmail.com>
2015-05-28 16:14:32 +0200

    Revert "QS: add Sync tile (1/2)"
    This reverts commit d823f172613b835e041df9087ec9314b69cb4bed.

--- core/java/com/android/internal/util/cm/QSConstants.java
+++ core/java/com/android/internal/util/cm/QSConstants.java
@@ -47,7 +47,6 @@ public class QSConstants {
     public static final String TILE_SCREEN_TIMEOUT = "screen_timeout";
     public static final String TILE_LIVE_DISPLAY = "live_display";
     public static final String TILE_SCREENSHOT = "screenshot";
-    public static final String TILE_SYNC = "sync";
     public static final String TILE_USB_TETHER = "usb_tether";
     public static final String TILE_HEADS_UP = "heads_up";
     public static final String TILE_AMBIENT_DISPLAY = "ambient_display";
